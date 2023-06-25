7cc0e02234a5824d6bc33bf471b7c7ae9e2d27cb
inaha <72016813+tsukinaha@users.noreply.github.com>
2023-06-25 11:34:10 +0800

    Update day.sh

--- day.sh
+++ day.sh
@@ -5,14 +5,13 @@ date=$(date --date="today" "+%Y-%m-%d")
 hostname=$(hostname)
 # Get local IP address
 local_ip=$(hostname -I | awk '{print $1}')
+chat_id=$1
+token=$2
 # Run vnstat command and save output to file
 vnstat -d -i enp3s0 > /tmp/vnstat.txt
 # Parse rx and tx values from vnstat output
 rx=$(vnstat -d -i enp3s0 | grep -A 1 "$(date --date="yesterday" "+%Y-%m-%d")" /tmp/vnstat.txt | head -n 1 |awk '{print $2, $3}' | tr '\n' ' ')
 tx=$(vnstat -d -i enp3s0 | grep -A 1 "$(date --date="yesterday" "+%Y-%m-%d")" /tmp/vnstat.txt | head -n 1 |awk '{print $5, $6}'| tr '\n' ' ')
-# Set Telegram bot token, chat ID, and message
-token="114514"
-chat_id="114514"
 url="https://api.telegram.org/bot${token}/sendMessage"
 message="[每日流量统计] ${date} ${time} ${hostname} (${local_ip}) 今日 出站 ${tx} 入站 ${rx}"
 # Send message to Telegram
